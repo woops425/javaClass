dd988bd4725748064a040697d090e4dd2b068a9f
woops425 <162817809+woops425@users.noreply.github.com>
2024-10-07 17:39:19 +0900

    Add files via upload

---
+++ Intro.java
@@ -0,0 +1,10 @@
+package week_02;
+public class Intro {
+    public static void main(String[] args) {
+        // [hw1_2] 자기소개 프로그램
+        System.out.println("hw1_2: 박상우");
+        System.out.println("자기소개 :");
+        System.out.println("글로벌경영학과 202010629 박상우입니다.");
+        System.out.println("수업에 열심히 임하겠습니다.");
+    }
+}
